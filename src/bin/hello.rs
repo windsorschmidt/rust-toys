2b1bdf350078e229ad274d3cbd63897362a44bec
Windsor Schmidt <windsor.schmidt@gmail.com>
2016-06-23 11:49:04 -0700

    add documentation comment

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,4 +1,10 @@
-fn main() {
+/// Prints a welcome message.
+///
+/// # A Heading
+/// Documentation comments start on a line with three slashes. Comments are
+/// written in Markdown, so we can have *bold* and **italic** text, etc.
+///
+pub fn main() {
     println!("Hello, world!");
 }
 
